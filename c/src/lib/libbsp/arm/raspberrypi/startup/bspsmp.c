cb52e828ae7484bb00277c719f3f76d26639c41f
Rohini Kulkarni <krohini1593@gmail.com>
2015-07-14 20:38:38 +0530

    Pi 2 SMP support added basic implementations of some required functions.

--- c/src/lib/libbsp/arm/raspberrypi/startup/bspsmp.c
+++ c/src/lib/libbsp/arm/raspberrypi/startup/bspsmp.c
@@ -1,13 +1,13 @@
-#include<raspberrypi2.h>
+#include <bsp/raspberrypi.h>
+#include <bsp/arm-cp15-start.h>
 
 void start_processor(uint32_t cpuid)
 {
-  uint32_t *write_set_reg = MAILBOX_WRITE_SET_BASE + 0x10*cpuid;
-  /* START_ADDRESS to be defined */
-  *write_set_reg | = START_ADDRESS;
+  void (*cpu_mailbox_write_set_reg)() = (void*)(MAILBOX_WRITE_SET_BASE + 0x10*cpuid); 
+  cpu_mailbox_write_set_reg = &_start;
 }
 
-void start_on_secondary_processor()
+void raspberrypi_wake_secondary_processors()
 {
   uint32_t cpu_count = 4;
   uint32_t cpuid;
@@ -15,5 +15,58 @@ void start_on_secondary_processor()
   for(cpuid=1;cpuid<cpu_count;cpuid++)
   {
     start_processor(cpuid);
+  }
+}
+
+BSP_START_TEXT_SECTION static inline void
+start_on_secondary_processor(void)
+{
+  uint32_t ctrl;
+
+  arm_a9mpcore_start_set_vector_base();
+
+  ctrl = arm_cp15_start_setup_mmu_and_cache(
+    0,
+    ARM_CP15_CTRL_AFE | ARM_CP15_CTRL_Z
+  );
+
+  arm_cp15_set_domain_access_control(
+    ARM_CP15_DAC_DOMAIN(ARM_MMU_DEFAULT_CLIENT_DOMAIN, ARM_CP15_DAC_CLIENT)
+  );
+
+  arm_cp15_set_translation_table_base(
+    (uint32_t *) bsp_translation_table_base
+  );
+
+  ctrl |= ARM_CP15_CTRL_I | ARM_CP15_CTRL_C | ARM_CP15_CTRL_M;
+  arm_cp15_set_control(ctrl);
+
+  _SMP_Start_multitasking_on_secondary_processor();
+}
+
+bool _CPU_SMP_Start_processor(uint32_t cpu_index)
+{
+  (void) cpu_index;
+
+  /* Nothing to do */
+
+  return true;
+}
+
+uint32_t _CPU_SMP_Initialize(void)
+{
+  uint32_t hardware_count = 4;
+  uint32_t linker_count = (uint32_t) bsp_processor_count;
+
+  return hardware_count <= linker_count ? hardware_count : linker_count;
+}
+
+void _CPU_SMP_Finalize_initialization(uint32_t cpu_count)
+{
+  /* this definition is incomplete */
+  if (cpu_count > 0) {
+    rtems_status_code sc;
+    
+    sc = RTEMS_SUCCESSFUL;
   }
 }
